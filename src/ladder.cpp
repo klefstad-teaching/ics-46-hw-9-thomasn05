59f39e0141383dbbc17b3787c65688761b3526a9
thomasn05 <Tuancalifornia714@yahoo.com>
2025-03-07 20:49:58 -0800

    generate word ladder

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -25,6 +25,36 @@ bool is_adjacent(const string &word1, const string &word2)
     return edit_distance_within(word1, word2, 1);
 }
 
+vector<string> generate_word_ladder(const string &begin_word, const string &end_word, const set<string> &word_list)
+{
+    queue<vector<string>> ladder_queue;
+    ladder_queue.push({begin_word});
+    set<string> visited;
+    visited.insert(begin_word);
+
+    while (!ladder_queue.empty())
+    {
+        vector<string> ladder = ladder_queue.front();
+        ladder_queue.pop();
+        string last_word = ladder.back();
+
+        for (const auto &word : word_list)
+        {
+            if (is_adjacent(last_word, word) && visited.find(word) == visited.end())
+            {
+                visited.insert(word);
+                auto new_ladder = ladder;
+                new_ladder.push_back(word);
+                if (word == end_word)
+                    return new_ladder;
+                ladder_queue.push(new_ladder);
+            }
+        }
+    }
+
+    return {};
+}
+
 void load_words(set<string> &word_list, const string &file_name)
 {
     ifstream in(file_name);
