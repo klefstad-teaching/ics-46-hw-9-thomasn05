d631e8e60883c425db964fee076264d996ee093e
thomasn05 <Tuancalifornia714@yahoo.com>
2025-03-10 15:21:55 -0700

    testing for word ladder

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -92,14 +92,24 @@ void print_word_ladder(const vector<string> &ladder)
     cout << endl;
 }
 
-void verify_word_ladder()
-{
+#define my_assert(e) {cout << #e << ((e) ? " passed": " failed") << endl;}
+
+void verify_word_ladder() {
+
     set<string> word_list;
+
     load_words(word_list, "src/words.txt");
-    vector<string> ladder1 = generate_word_ladder("car", "cheat", word_list);
-    print_word_ladder(ladder1);
-    vector<string> ladder2 = generate_word_ladder("code", "data", word_list);
-    print_word_ladder(ladder2);
-    vector<string> ladder3 = generate_word_ladder("Marty", "curls", word_list);
-    print_word_ladder(ladder3);
+
+    my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
+
+    my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
+
+    my_assert(generate_word_ladder("code", "data", word_list).size() == 6);
+
+    my_assert(generate_word_ladder("work", "play", word_list).size() == 6);
+
+    my_assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
+
+    my_assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
+
 }
