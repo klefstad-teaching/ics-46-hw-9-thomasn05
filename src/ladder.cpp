746e5f3fa149aa21be8efa80b98c30ebaea31233
thomasn05 <Tuancalifornia714@yahoo.com>
2025-03-10 14:40:00 -0700

    ladder main

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -96,6 +96,10 @@ void verify_word_ladder()
 {
     set<string> word_list;
     load_words(word_list, "src/words.txt");
-    vector<string> ladder = generate_word_ladder("car", "cheat", word_list);
-    print_word_ladder(ladder);
+    vector<string> ladder1 = generate_word_ladder("car", "cheat", word_list);
+    print_word_ladder(ladder1);
+    vector<string> ladder2 = generate_word_ladder("code", "data", word_list);
+    print_word_ladder(ladder2);
+    vector<string> ladder3 = generate_word_ladder("Marty", "curls", word_list);
+    print_word_ladder(ladder3);
 }
